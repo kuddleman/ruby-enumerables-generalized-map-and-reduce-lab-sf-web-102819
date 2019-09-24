e44ec2d146223c61225848345490ffd520602f5a
Donald Lobree <edl1155@gmail.com>
2019-09-24 22:01:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def map(element1, &block)
+  element1.map(&block)
+end
+
+# def reduce(element1, starting_point = 0)
+#   element1.reduce(starting_point) { |sum, num| sum + num }
+# end
+
+def reduce_to_total(source_array, starting_point = 0)
+    source_array.reduce(starting_point) { |sum, num| sum + num}
+end
