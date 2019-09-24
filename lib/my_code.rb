624c0c2f32fa04620b05decd3b0b850b09a68cfc
Donald Lobree <edl1155@gmail.com>
2019-09-24 22:20:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,7 +7,9 @@ end
 #   element1.reduce(starting_point) { |sum, num| sum + num }
 # end
 
-def my_own_reduce(source_array, starting_point = 0)
-    source_array.reduce(starting_point) { |sum, num| sum + num}
-    sum
+def my_own_reduce(array, starting_point = 0)
+   final_total =  array.reduce(starting_point) { |sum, num| sum + num}
+   yield
+   final_total
+    
 end
